ee6537e8ffb0b707a2cf37b20d8b554b24ff3621
typicalamna <typicalamna@gmail.com>
2021-04-25 12:07:20 -0400

    changing name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
-import Weather from "./Weather";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-export default function App() {
+function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Weather defaultCity="New York" />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
+
+export default App;
